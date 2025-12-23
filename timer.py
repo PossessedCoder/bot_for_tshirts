e65c798faf448d894560d070c234f48a64000384
PossessedCoder <dmitrijnikolae67@gmail.com>
2025-12-23 11:30:12 +0300

    Финал

---
+++ timer.py
@@ -0,0 +1,30 @@
+import asyncio
+from datetime import timedelta
+
+from aiogram.types import Message
+
+from main import simple_inline
+import datetime
+from dotenv import load_dotenv
+from os import getenv
+from aiogram import Bot, Dispatcher
+from aiogram import F
+
+load_dotenv('.env')
+TOKEN = getenv("TOKEN")
+bot = Bot(token=getenv('TOKEN'))
+dp = Dispatcher(bot=bot)
+global_time = (datetime.datetime(year=2025, day=30, month=12) + timedelta(hours=19) - datetime.datetime.now())
+@dp.message(F.text == '/start')
+async def start(message: Message):
+    global global_time
+    global_time = (datetime.datetime(year=2025, day=30, month=12) + timedelta(hours=19) - datetime.datetime.now())
+    a = await message.answer('Отсчёт уже пошёл...', reply_markup=await simple_inline([[[str(global_time), 'city_selection']]]))
+
+async def main() -> None:
+    print(await bot.get_me())
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
