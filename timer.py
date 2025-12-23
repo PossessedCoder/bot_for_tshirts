7a433279d8c421c3b0269039c3299cd2a4c81392
PossessedCoder <dmitrijnikolae67@gmail.com>
2025-12-23 17:25:43 +0300

    Финал

--- timer.py
+++ timer.py
@@ -19,12 +19,7 @@ global_time = (datetime.datetime(year=2025, day=30, month=12) - datetime.datetim
 async def start(message: Message):
     global global_time
     global_time = (datetime.datetime(year=2025, day=30, month=12) - datetime.datetime.now())
-    days = global_time.days
-    global_time = str(global_time).split(',')
-    global_time = global_time[1].split('.')[0]
-    global_time = ':'.join((str(int(global_time.split(':')[0]) + days * 24), global_time.split(':')[1], global_time.split(':')[2]))
-    print(global_time)
-    a = await message.answer('Отсчёт уже пошёл...', reply_markup=await simple_inline([[[global_time, 'city_selection']]]))
+    a = await message.answer('Отсчёт уже пошёл...', reply_markup=await simple_inline([[[str(global_time).replace('days,', 'дней').split('.')[0], 'city_selection']]]))
 
 async def main() -> None:
     print(await bot.get_me())
