f150a16c54c2104ee591582bedb580c96f5a53b2
PossessedCoder <dmitrijnikolae67@gmail.com>
2025-12-23 17:28:59 +0300

    Финал

--- timer.py
+++ timer.py
@@ -14,11 +14,11 @@ load_dotenv('.env')
 TOKEN = getenv("TOKEN")
 bot = Bot(token=getenv('TOKEN'))
 dp = Dispatcher(bot=bot)
-global_time = (datetime.datetime(year=2025, day=30, month=12) - datetime.datetime.now())
+global_time = (datetime.datetime(year=2025, day=28, month=12) - datetime.datetime.now())
 @dp.message(F.text == '/start')
 async def start(message: Message):
     global global_time
-    global_time = (datetime.datetime(year=2025, day=30, month=12) - datetime.datetime.now())
+    global_time = (datetime.datetime(year=2025, day=28, month=12) - datetime.datetime.now())
     a = await message.answer('Отсчёт уже пошёл...', reply_markup=await simple_inline([[[str(global_time).replace('days,', 'дней').split('.')[0], 'city_selection']]]))
 
 async def main() -> None:
