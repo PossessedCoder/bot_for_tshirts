f9ae078f13b2c08920bebd92a5b482bff0a56404
PossessedCoder <dmitrijnikolae67@gmail.com>
2025-12-23 17:18:07 +0300

    Финал

--- timer.py
+++ timer.py
@@ -14,12 +14,17 @@ load_dotenv('.env')
 TOKEN = getenv("TOKEN")
 bot = Bot(token=getenv('TOKEN'))
 dp = Dispatcher(bot=bot)
-global_time = (datetime.datetime(year=2025, day=30, month=12) + timedelta(hours=19) - datetime.datetime.now())
+global_time = (datetime.datetime(year=2025, day=30, month=12) - datetime.datetime.now())
 @dp.message(F.text == '/start')
 async def start(message: Message):
     global global_time
-    global_time = (datetime.datetime(year=2025, day=30, month=12) + timedelta(hours=19) - datetime.datetime.now())
-    a = await message.answer('Отсчёт уже пошёл...', reply_markup=await simple_inline([[[str(global_time), 'city_selection']]]))
+    global_time = (datetime.datetime(year=2025, day=30, month=12) - datetime.datetime.now())
+    days = global_time.days
+    global_time = str(global_time).split(',')
+    global_time = global_time[1].split('.')[0]
+    global_time = ':'.join((str(int(global_time.split(':')[0]) + days * 24), global_time.split(':')[1], global_time.split(':')[2]))
+    print(global_time)
+    a = await message.answer('Отсчёт уже пошёл...', reply_markup=await simple_inline([[[global_time, 'city_selection']]]))
 
 async def main() -> None:
     print(await bot.get_me())
