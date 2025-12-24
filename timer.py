a1f0d84cece0ad85e050cc0e116580b428b73027
PossessedCoder <dmitrijnikolae67@gmail.com>
2025-12-24 22:32:42 +0300

    Финал

--- timer.py
+++ timer.py
@@ -15,12 +15,19 @@ TOKEN = getenv("TOKEN")
 bot = Bot(token=getenv('TOKEN'))
 dp = Dispatcher(bot=bot)
 global_time = (datetime.datetime(year=2025, day=28, month=12) - datetime.datetime.now())
+
+
 @dp.message(F.text == '/start')
 async def start(message: Message):
     global global_time
     global_time = (datetime.datetime(year=2025, day=28, month=12) - datetime.datetime.now())
-    a = await bot.send_photo(message.from_user.id,photo=BufferedInputFile(bytes(open(f'photos/timer.jpg', 'rb').read()), 'Candle'), reply_markup=await simple_inline([[['ОТСЧЁТ УЖЕ ПОШЁЛ', 'https://t.me/renaissanceoutfit|url']]]))
-    b = await message.answer('<b>ТАЙМЕР           </b>',parse_mode='HTML', reply_markup= await simple_inline([[[str(global_time).replace('days,', 'дня').split('.')[0], 'https://t.me/renaissanceoutfit|url']]]))
+    a = await bot.send_photo(message.from_user.id,
+                             photo=BufferedInputFile(bytes(open(f'photos/timer.jpg', 'rb').read()), 'Candle'),
+                             reply_markup=await simple_inline(
+                                 [[['ОТСЧЁТ УЖЕ ПОШЁЛ', 'https://t.me/renaissanceoutfit|url']]]))
+    b = await message.answer('<b>ОСТАЛОСЬ           </b>', parse_mode='HTML', reply_markup=await simple_inline(
+        [[[str(global_time).replace('days,', 'дня').split('.')[0], 'https://t.me/renaissanceoutfit|url']]]))
+
 
 async def main() -> None:
     print(await bot.get_me())
